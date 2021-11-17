9052340805af051a3f879ebe62419f11e0d7d7af
orszaczkyanna <orszaczky.anna@gmail.com>
2021-11-17 17:00:00 +0100

    HTML, CSS gyakorlás - képformázás és dinamikus tartalom

---
+++ 1. félév/07 HTML, CSS gyakorlás - képformázás és dinamikus tartalom/feladat - sárgatúró/hozzavalo.js
@@ -0,0 +1,6 @@
+function recept() {
+  var i = document.getElementById("fo").value;
+  document.getElementById("tej").innerText = i / 5;
+  document.getElementById("tojas").innerText = i * 2;
+  document.getElementById("cukor").innerText = i * 1;
+}
